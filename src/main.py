2671a9b86660e880acf40f022e45760d796073dc
Reuben Haug <reuben.haug@gmail.com>
2025-04-07 18:20:16 -0700

    Stub main function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 # src/main.py
-from textnode import TextNode, TextType
 
 def main():
-    testNode = TextNode("This is some text", TextType.TEXT)
+    pass
 
-    print(testNode.__repr__())
+
 if __name__ == "__main__":
     main()
